d3ea7c7dd68bae2b59c497a9fcef7a11f38141ac
prostars <prostars@gmail.com>
2018-04-22 11:19:38 +0900

    change list to array

--- Main.java
+++ Main.java
@@ -5,71 +5,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<List<Integer>> matrix = new ArrayList<List<Integer>>();
-        matrix.add(new ArrayList<Integer>());
-        matrix.add(new ArrayList<Integer>());
-        matrix.add(new ArrayList<Integer>());
-        matrix.add(new ArrayList<Integer>());
-        matrix.add(new ArrayList<Integer>());
-        matrix.add(new ArrayList<Integer>());
-        matrix.add(new ArrayList<Integer>());
-        matrix.get(0).add(0);
-        matrix.get(0).add(1);
-        matrix.get(0).add(1);
-        matrix.get(0).add(0);
-        matrix.get(0).add(0);
-        matrix.get(0).add(0);
-        matrix.get(0).add(0);
+        int[][] matrix =
+                {
+                        {0, 1, 1, 0, 0, 0, 0},
+                        {1, 0, 0, 0, 1, 0, 0},
+                        {1, 0, 0, 0, 0, 1, 0},
+                        {1, 0, 0, 0, 0, 0, 0},
+                        {0, 1, 0, 0, 0, 1, 1},
+                        {0, 0, 1, 0, 1, 0, 0},
+                        {0, 0, 0, 0, 0, 1, 0}
+                };
 
-        matrix.get(1).add(1);
-        matrix.get(1).add(0);
-        matrix.get(1).add(0);
-        matrix.get(1).add(0);
-        matrix.get(1).add(1);
-        matrix.get(1).add(0);
-        matrix.get(1).add(0);
-
-        matrix.get(2).add(1);
-        matrix.get(2).add(0);
-        matrix.get(2).add(0);
-        matrix.get(2).add(0);
-        matrix.get(2).add(0);
-        matrix.get(2).add(1);
-        matrix.get(2).add(0);
-
-        matrix.get(3).add(1);
-        matrix.get(3).add(0);
-        matrix.get(3).add(0);
-        matrix.get(3).add(0);
-        matrix.get(3).add(0);
-        matrix.get(3).add(0);
-        matrix.get(3).add(0);
-
-        matrix.get(4).add(0);
-        matrix.get(4).add(1);
-        matrix.get(4).add(0);
-        matrix.get(4).add(0);
-        matrix.get(4).add(0);
-        matrix.get(4).add(1);
-        matrix.get(4).add(1);
-
-        matrix.get(5).add(0);
-        matrix.get(5).add(0);
-        matrix.get(5).add(1);
-        matrix.get(5).add(0);
-        matrix.get(5).add(1);
-        matrix.get(5).add(0);
-        matrix.get(5).add(0);
-
-        matrix.get(6).add(0);
-        matrix.get(6).add(0);
-        matrix.get(6).add(0);
-        matrix.get(6).add(0);
-        matrix.get(6).add(0);
-        matrix.get(6).add(1);
-        matrix.get(6).add(0);
-
-        GraphUsingMatrix graph = new GraphUsingMatrix(matrix);
+        DirectedGraphUsingMatrix graph = new DirectedGraphUsingMatrix(matrix);
         graph.dfs(0);
         System.out.println("-----------------------------------");
         graph.dfsMatrix();
